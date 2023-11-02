b37195bea1b5cd189f38afcc5e7fb7e6b8ff1f03
Gachick <zte.a510.artem@gmail.com>
2023-11-03 00:17:49 +0300

    0.0.4

--- Keyboards/keyboards.py
+++ Keyboards/keyboards.py
@@ -1,17 +1,29 @@
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-def reply_keyboard():
-    reply_keyboard = ReplyKeyboardBuilder()
-    reply_keyboard.button(text='/write_training')
-    reply_keyboard.button(text='/del_training')
-    reply_keyboard.button(text='/help')
-    reply_keyboard.button(text='/show_all')
-    reply_keyboard.adjust(2, 1, 1)
-    return reply_keyboard.as_markup(resize_keyboard=True, one_time_keyboard=True)
+class UserKeyboards:
+    @staticmethod
+    def reply_keyboard():
+        reply_keyboard = ReplyKeyboardBuilder()
+        reply_keyboard.button(text='/help')
+        reply_keyboard.button(text='/show_all_tasks')
+        reply_keyboard.adjust(1)
+        return reply_keyboard.as_markup(resize_keyboard=True, one_time_keyboard=True)
+    
+    @staticmethod
+    def reply_keyboard_answer():
+        reply_keyboard = ReplyKeyboardBuilder()
+        reply_keyboard.button(text='Да')
+        reply_keyboard.button(text='Нет')
+        reply_keyboard.adjust(2)
+        return reply_keyboard.as_markup(resize_keyboard=True, one_time_keyboard=True)
 
-def reply_keyboard_answer():
-    reply_keyboard = ReplyKeyboardBuilder()
-    reply_keyboard.button(text='Да')
-    reply_keyboard.button(text='Нет')
-    reply_keyboard.adjust(2)
-    return reply_keyboard.as_markup(resize_keyboard=True, one_time_keyboard=True)
+class AdminKeyboards:
+    @staticmethod
+    def reply_keyboard():
+        reply_keyboard = ReplyKeyboardBuilder()
+        reply_keyboard.button(text='/add_task')
+        reply_keyboard.button(text='/del_task')
+        reply_keyboard.button(text='/help')
+        reply_keyboard.button(text='/show_all_tasks')
+        reply_keyboard.adjust(2, 1, 1)
+        return reply_keyboard.as_markup(resize_keyboard=True, one_time_keyboard=True)
